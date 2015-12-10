311f3cc97dd6e90b5fe069e508208cc157b87cb5
arcturus611 <swati.6.itaws@gmail.com>
2015-12-10 07:54:26 -0800

    Corrected data being randomized for every averaging instance.
    To check the MSE, we are averaging over several instances, but in each
    instance, only the noise matrix should be new. The data needs to be the
    same.

--- noisy_est_sim_str_convex.m
+++ noisy_est_sim_str_convex.m
@@ -1,15 +1,14 @@
-
 function Empirical_MSE_Vec = noisy_est_sim_str_convex()
-%% Set initial parameters
+    %% Set initial parameters
     [n, k_vals, Gamma, Empirical_MSE_Vec, NUM_AVGS, lambda1, lambda2] = set_init_params();
 
     %% Iterate over all k values
     k_count = 1;
     for k = k_vals
         Empirical_MSE_Raw = zeros(NUM_AVGS, 1); 
+        %% Generate data
+        X0 = generate_data(n, k);
         for avg_idx = 1:NUM_AVGS 
-            %% Generate data
-            X0 = generate_data(n, k);
             %% Creating noise matrix
             Z = randn(n, n); %noise matrix
             %% Create overall matrix
@@ -20,15 +19,9 @@ function Empirical_MSE_Vec = noisy_est_sim_str_convex()
                 minimize(0.5*square_pos(norm( Y - X , 'fro')) + lambda1*norm(X, 1) + lambda2*norm_nuc(X))
             cvx_end
             
-        %% Empirical MSE Calculation
+            %% Empirical MSE Calculation
             Err = X - Gamma*X0; 
             Empirical_MSE_Raw = sum(Err(:).^2);
-%             for p = 1:n
-%                 for q = 1:n
-%                     Empirical_MSE_Raw(avg_idx) = Empirical_MSE_Raw(avg_idx) + Err(p, q)^2;
-%                 end
-%             end
-            
         end
         
         Empirical_MSE_Vec(k_count) = mean(Empirical_MSE_Raw);
--- set_init_params.m
+++ set_init_params.m
@@ -1,9 +1,9 @@
 function [n, k_vals, Gamma, Empirical_MSE_Vec, NUM_AVGS, lambda1, lambda2] = set_init_params()
 n = 30;
-k_vals = 3:13; 
+k_vals = 3:10; 
 Gamma = 10^2.5; % [10^0 10^0.5 10^1 10^1.5 10^2]; %Signal to Noise Ratio
 Empirical_MSE_Vec = zeros(numel(k_vals), 1); %MSE for each value of k
 NUM_AVGS = 200; %number of MSE values to be averaged 
-lambda1 = 0.8;
-lambda2 = 5; 
+lambda1 = 0;
+lambda2 = 8; 
 end
